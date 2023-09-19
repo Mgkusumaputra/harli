7ebea9bcefa2b9c75a0f320274eb81b57782a643
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2023-09-19 20:16:20 +0700

    🐛 fix: api query for filter month

--- app/api/route.ts
+++ app/api/route.ts
@@ -5,20 +5,21 @@ import { NextResponse } from "next/server";
 const __dirname = path.join(process.cwd(), "/data/");
 
 export async function GET(request: Request) {
-  const checkParams = new URL(request.url);
-  const yearParams = checkParams.searchParams.get("year");
-  const monthParams = checkParams.searchParams.get("month");
-  const includeCutiParams = checkParams.searchParams.get("includeCuti");
-
   try {
+    const checkParams = new URL(request.url);
+    const yearParams = checkParams.searchParams.get("year");
+    const monthParams = checkParams.searchParams.get("month");
+    const includeCutiParams = checkParams.searchParams.get("includeCuti");
+
     let result = [];
     let currentYear = new Date().getFullYear();
     let data = await fs.readFile(
       path.join(__dirname, `${currentYear}.json`),
       "utf-8"
     );
-    
+
     if (yearParams) {
+      currentYear = parseInt(yearParams);
       data = await fs.readFile(
         path.join(__dirname, `${yearParams}.json`),
         "utf-8"
@@ -27,23 +28,19 @@ export async function GET(request: Request) {
 
     let parseResult = JSON.parse(data);
 
-    if (monthParams && yearParams) {
+    if (monthParams && currentYear) {
       result = parseResult.filter((item: { date: string | number | Date }) => {
-        if ((new Date(item.date).getMonth() + 1).toString() === monthParams) {
+        if (new Date(item.date).getMonth() + 1 === parseInt(monthParams)) {
           return item;
         }
       });
-    } 
-
-    if (includeCutiParams) {
-      result = parseResult.filter((item: { include_cuti: boolean; }) => {
+    } else if (includeCutiParams) {
+      result = parseResult.filter((item: { include_cuti: boolean }) => {
         if (item.include_cuti === true) {
           return item;
         }
       });
-    }
-
-    else if (currentYear) {
+    } else if (currentYear) {
       result = parseResult;
     }
 
