a114d606204911a6e0ed87c87f67417fb1a6c788
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2023-09-24 19:47:07 +0700

    🐛 fix: fetching API error in production

--- utils/env.ts
+++ utils/env.ts
@@ -1,8 +1,4 @@
 const siteURL = process.env.NEXT_PUBLIC_SITE_URL;
 
 export const SITE_URL = siteURL;
-export const BASE_URL =
-  process.env.NODE_ENV === "development" ||
-  process.env.NODE_ENV === "production"
-    ? "http://[::1]:3000"
-    : siteURL;
+export const BASE_URL = "http://[::1]:3000";
